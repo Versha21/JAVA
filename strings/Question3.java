206e6426e1de173d72baca02d9f4ad0348c4900f
Versha Dewangan <vershadewangan8@gmail.com>
2026-09-12 21:05:41 +0530

    update

--- strings/Question3.java
+++ strings/Question3.java
@@ -1,7 +1,11 @@
 package strings;
 import java.util.Scanner;
 public class Question3 {
-    static void main(String[] args) {
-
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter a string: ");
+        String input = scanner.nextLine();
+        System.out.println("You entered: " + input);
+        scanner.close();
     }
 }
